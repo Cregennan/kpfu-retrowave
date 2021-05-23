cfaf90cb3d8cb916988a798bbf44c44bc2ded78a
RadioNurshat <silver2prod@gmail.com>
2021-05-23 17:55:30 +0300

    Preloader init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        //$("#preloader").addClass('fadeout');
+        //$("#preloader").hide();
+        //$(html).removeClass('no-scroll');
+
+
+
+    }, 4000)
+
+})
