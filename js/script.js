1f811e9da87d3f69dc7f0f296b1539fe43405311
RadioNurshat <silver2prod@gmail.com>
2021-05-24 12:09:25 +0300

    Music player ini

--- js/script.js
+++ js/script.js
@@ -7,3 +7,25 @@ $(document).ready(function() {
         }, 1000)
     }, 3000)
 })
+
+const audio_open = () => {
+    $('html').addClass('no-scroll');
+    $("#audio-suggestion").show();
+}
+const audio_close = () => {
+    $("#audio-suggestion").hide();
+    $('html').removeClass('no-scroll');
+}
+
+var audio = new Audio('./audio/white_night1.mp3');
+
+setTimeout(function() {
+    audio_open();
+}, 1000)
+
+$(document).on('click', "#turn_on_music", function() {
+    audio.play();
+})
+$(document).on('click', "#turn_off_music", function() {
+    audio.pause();
+})
